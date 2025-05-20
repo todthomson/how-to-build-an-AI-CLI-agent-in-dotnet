f0c54d107fbcc250c6dd6a9195c293db862e7355
Tod Thomson <todthomson@me.com>
2025-05-20 11:33:31 +1000

    import: main.go from...
    https://ampcode.com/how-to-build-an-agent

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"github.com/anthropics/anthropic-sdk-go"
+)
+
+func main() {
+	client := anthropic.NewClient()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	getUserMessage := func() (string, bool) {
+		if !scanner.Scan() {
+			return "", false
+		}
+		return scanner.Text(), true
+	}
+
+	agent := NewAgent(&client, getUserMessage)
+	err := agent.Run(context.TODO())
+
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
+}
+
+func NewAgent(client anthropic.Client, getUserMessage func() (string, bool)) Agent {
+	return &Agent{
+		client: client,
+		getUserMessage: getUserMessage,
+	}
+}
+
+type Agent struct {
+	client *anthropic.Client
+	getUserMessage func() (string, bool)
+}
